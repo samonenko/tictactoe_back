b626c6678a23fc09368fb34862e45cedc1da7b6b
samonenko <samonenko.i@gmail.com>
2023-12-30 12:24:28 +0300

    modified:   app/public/index.php

--- app/public/index.php
+++ app/public/index.php
@@ -2,10 +2,8 @@
     include_once('game.php');
 ?>
 <!DOCTYPE html>
-<html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Крестики-нолики</title>
 </head>
 <body>
